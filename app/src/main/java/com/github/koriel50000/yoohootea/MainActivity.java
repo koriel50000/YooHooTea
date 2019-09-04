89a783f0d104556b4aaaa124d2c231ed51a82fd3
koriel50000 <JZW00040@gmail.com>
2019-09-05 00:18:42 +0900

    音声認識を追加

--- app/src/main/java/com/github/koriel50000/yoohootea/MainActivity.java
+++ app/src/main/java/com/github/koriel50000/yoohootea/MainActivity.java
@@ -1,28 +1,34 @@
 package com.github.koriel50000.yoohootea;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.bassaer.chatmessageview.model.Message;
 import com.github.bassaer.chatmessageview.view.MessageView;
 
+import ai.api.AIConfiguration;
+import ai.api.AIListener;
+import ai.api.AIService;
+import ai.api.RequestExtras;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Metadata;
+import ai.api.model.Result;
+import ai.api.model.Status;
 import ai.kitt.snowboy.AppResCopy;
 import ai.kitt.snowboy.MsgEnum;
 import ai.kitt.snowboy.audio.AudioDataSaver;
 import ai.kitt.snowboy.audio.RecordingThread;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AIListener {
 
     public static final String TAG = MainActivity.class.getName();
 
@@ -33,17 +39,43 @@ public class MainActivity extends AppCompatActivity {
     private User myAccount;
     private User yooHooBot;
 
+    private AIService aiService;
+    private TextToSpeech textToSpeech;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initAIService();
+        initTextToSpeech();
         initMessageView();
 
         setProperVolume();
         AppResCopy.copyResFromAssetsToSD(this);
         recordingThread = new RecordingThread(handle, new AudioDataSaver());
         recordingThread.startRecording();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // use this method to disconnect from speech recognition service
+        // Not destroying the SpeechRecognition object in onPause method would block other apps from using SpeechRecognition service
+        if (aiService != null) {
+            aiService.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // use this method to reinit connection to recognition service
+        if (aiService != null) {
+            aiService.resume();
+        }
     }
 
     @Override
@@ -105,24 +137,182 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void hotwordDetected() {
-        Message sendMessage = new Message.Builder()
-                .setUser(myAccount)
-                .setRightMessage(true)
-                .setMessageText("おーい")
-                .build();
-        messageView.setMessage(sendMessage);
+        recordingThread.stopRecording();
 
-        final Message receivedMessage = new Message.Builder()
-                .setUser(yooHooBot)
-                .setRightMessage(false)
-                .setMessageText("？？？")
-                .build();
+        aiService.startListening();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                messageView.setMessage(receivedMessage);
+                aiService.stopListening();
             }
         }, 2000);
+    }
+
+    private void callMessage(final String speech) {
+        Message callMessage = new Message.Builder()
+                .setUser(myAccount)
+                .setRightMessage(true)
+                .setMessageText("おーい " + speech)
+                .build();
+        messageView.setMessage(callMessage);
+        messageView.scrollToEnd();
+
+        // TODO dialogflow
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        replyMessage("おーい " + speech);
+                    }
+                }, 2000);
+    }
+
+    private void replyMessage(String speech) {
+        final Message replyMessage = new Message.Builder()
+                .setUser(yooHooBot)
+                .setRightMessage(false)
+                .setMessageText(speech)
+                .build();
+        messageView.setMessage(replyMessage);
+        messageView.scrollToEnd();
+
+        speak(speech);
+        // TODO utteranceId callback
+        new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        recordingThread.startRecording();
+                    }
+                }, 2000);
+    }
+
+    private void initAIService() {
+        final AIConfiguration config = new AIConfiguration(
+                "6cab6813dc8c416f92c3c2e2b4a7bc27",
+                AIConfiguration.SupportedLanguages.fromLanguageTag("ja"),
+                AIConfiguration.RecognitionEngine.System);
+
+        if (aiService != null) {
+            aiService.pause();
+        }
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+    }
+
+    @Override
+    public void onResult(final AIResponse response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "onResult");
+
+                //resultTextView.setText(gson.toJson(response));
+
+                Log.i(TAG, "Received success response");
+
+                // this is example how to get different parts of result object
+                final Status status = response.getStatus();
+                Log.i(TAG, "Status code: " + status.getCode());
+                Log.i(TAG, "Status type: " + status.getErrorType());
+
+                final Result result = response.getResult();
+                Log.i(TAG, "Resolved query: " + result.getResolvedQuery());
+
+                Log.i(TAG, "Action: " + result.getAction());
+
+                final String speech = result.getFulfillment().getSpeech();
+                Log.i(TAG, "Speech: " + speech);
+
+                callMessage(result.getResolvedQuery());
+
+                final Metadata metadata = result.getMetadata();
+                if (metadata != null) {
+                    Log.i(TAG, "Intent id: " + metadata.getIntentId());
+                    Log.i(TAG, "Intent name: " + metadata.getIntentName());
+                }
+
+//                final HashMap<String, JsonElement> params = result.getParameters();
+//                if (params != null && !params.isEmpty()) {
+//                    Log.i(TAG, "Parameters: ");
+//                    for (final Map.Entry<String, JsonElement> entry : params.entrySet()) {
+//                        Log.i(TAG, String.format("%s: %s", entry.getKey(), entry.getValue().toString()));
+//                    }
+//                }
+            }
+        });
+    }
+
+    @Override
+    public void onError(final AIError error) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, error.toString());
+            }
+        });
+    }
+
+    @Override
+    public void onAudioLevel(final float level) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                float positiveLevel = Math.abs(level);
+
+                if (positiveLevel > 100) {
+                    positiveLevel = 100;
+                }
+                Log.i(TAG, String.format("onAudioLevel: %s", positiveLevel));
+            }
+        });
+    }
+
+    @Override
+    public void onListeningStarted() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, "onListeningStarted");
+            }
+        });
+    }
+
+    @Override
+    public void onListeningCanceled() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, "onListeningCanceled");
+            }
+        });
+    }
+
+    @Override
+    public void onListeningFinished() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG,"onListeningFinished");
+            }
+        });
+    }
+
+    private void initTextToSpeech() {
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (TextToSpeech.SUCCESS == status) {
+                    Log.d(TAG, "initialized");
+                } else {
+                    Log.e(TAG, "failed to initialize");
+                }
+            }
+        });
+    }
+
+    private void speak(final String text) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
